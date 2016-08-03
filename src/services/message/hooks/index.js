8570750178dab7e7943436416e40097c8be8aef7
Jeffrey Arts <jeffrey.arts@philips.com>
2016-08-03 22:29:55 +0200

    Add hooks so creating messages are restricted to the users

--- src/services/message/hooks/index.js
+++ src/services/message/hooks/index.js
@@ -1,25 +1,37 @@
 'use strict';
 
-const globalHooks = require('../../../hooks');
+const restrictToSender = require('./restrict-to-sender');
+
+const auth = require('feathers-authentication').hooks;
 const hooks = require('feathers-hooks');
 
+const process = require('./process');
+const globalHooks = require('../../../hooks');
+const populateSender = hooks.populate('sentBy', {
+    service: 'users',
+    field: 'userId'
+});
 
 exports.before = {
-  all: [],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: []
+    all: [
+        auth.verifyToken(),
+        auth.populateUser(),
+        auth.restrictToAuthenticated()
+    ],
+    find: [],
+    get: [],
+    create: [process(), restrictToSender()],
+    update: [hooks.remove('sentBy')],
+    patch: [hooks.remove('sentBy')],
+    remove: []
 };
 
 exports.after = {
-  all: [],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: []
+    all: [],
+    find: [populateSender],
+    get: [populateSender],
+    create: [populateSender],
+    update: [],
+    patch: [],
+    remove: []
 };
